a059284782cc3402cba3859eb010e16a59c7ecd3
MahmoudHefny2001 <hefny4@gmail.com>
2023-01-31 11:22:29 +0200

    Initial commit

---
+++ cosmetics_shop/customer/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+route = DefaultRouter()
+
+# route.register(r'', views.)
---
+++ cosmetics_shop/product/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+
+
+
