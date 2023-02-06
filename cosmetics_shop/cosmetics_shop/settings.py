16d8b275563e418358ab41f6f520ed9881c477a9
MahmoudHefny2001 <hefny4@gmail.com>
2023-02-06 17:21:20 +0200

    added whitenoise for static files in production

--- cosmetics_shop/cosmetics_shop/settings.py
+++ cosmetics_shop/cosmetics_shop/settings.py
@@ -289,3 +289,5 @@ AUTHENTICATION_BACKENDS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
