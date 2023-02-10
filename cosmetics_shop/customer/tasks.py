708e2fdbe0c79c7fe7a980b63f7cfd08841071e1
MahmoudHefny2001 <hefny4@gmail.com>
2023-02-11 00:32:34 +0200

    Added some project configs

--- cosmetics_shop/customer/tasks.py
+++ cosmetics_shop/customer/tasks.py
@@ -4,5 +4,5 @@ from django.core.mail import send_mail
 
 @shared_task
 def send_registration_mail():
-    send_mail('Registration mail', 'Thanks for using our services', 'hefny4@gmail.com', 'mhmwdhfny22@fastmail.com')
+    send_mail('Registration mail', 'Thanks for using our services', 'hefny4@gmail.com', 'mhmwdhfny22@gmail.com')
     return None
--- cosmetics_shop/online_shop/celery.py
+++ cosmetics_shop/online_shop/celery.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cosmetics_shop.settings")
-app = Celery("cosmetics_shop")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "online_shop.settings")
+app = Celery("online_shop")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
