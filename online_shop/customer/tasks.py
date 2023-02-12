aacfa96e1510b1b87ded074ea311e37c1988e0ea
MahmoudHefny2001 <hefny4@gmail.com>
2023-02-12 16:47:40 +0200

    Last configs

--- online_shop/customer/tasks.py
+++ online_shop/customer/tasks.py
@@ -7,5 +7,6 @@ from django.core.mail import send_mail, send_mass_mail
 
 @shared_task
 def send_registration_mail(email):
+    print(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD, settings.EMAIL_HOST)
     send_mail('Registration mail', 'Thanks for using our services', settings.EMAIL_HOST_USER, [email,], fail_silently=False)
     return None
--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DEBUG', None)
+DEBUG = bool(os.environ.get('DEBUG', None))
 
 ALLOWED_HOSTS = ['*']
 
@@ -98,6 +98,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
 
@@ -322,9 +324,9 @@ CELERY_RESULT_SERIALIZER = 'json'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_USE_TLS = bool(os.environ.get('EMAIL_USE_TLS'))
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = False
+# EMAIL_USE_SSL = bool(os.environ.get(EMAIL_USE_SSL))
